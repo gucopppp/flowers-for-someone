e446ecc56b9ed34e58d91509da1be72f731c4e0a
gucopppp <gucopp16@gmail.com>
2025-05-22 02:03:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('HABEDEEE CIAA KEBAB YANG KE 16 RIBU TAHUN').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
